2a4434c3cf2a3dd9fb4c640367beef9e1793cd6a
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-17 17:44:54 +0530

    Linked List Added

---
+++ LinkList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkList<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        public void AddFirst(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            
+            if (head == null) 
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        public void Display()
+        { 
+            Node<T> temp= head;
+            while (temp != null) 
+            {       
+                Console.WriteLine(temp.data + "->");
+                temp = temp.next;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LinkedList
+{
+    class Program 
+    {
+
+        public static void Main(String[] args)
+        {
+
+            LinkList<int> l1 = new LinkList<int>();
+            l1.AddFirst(1);
+            l1.AddFirst(2);
+            
+            l1.Display();
+        
+        
+        }
+    }
+
+
+
+}
