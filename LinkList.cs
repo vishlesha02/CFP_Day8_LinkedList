5f7d92c92953268c95d013e1785d97df8c1634a3
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-23 23:44:23 +0530

    Add After Added

--- LinkList.cs
+++ LinkList.cs
@@ -43,6 +43,26 @@ namespace LinkedList
             }
         }
 
+        public bool AddAfter(T searchData, T addData)
+        {
+            Node<T> newNode = new Node<T>(addData);
+            Node<T> temp = head;
+
+            while (temp != null)
+            {
+                if (temp.data.Equals(searchData))
+                {
+                    newNode.next = temp.next;
+                    temp.next = newNode;
+                    return true;
+                }
+                temp = temp.next;
+            }
+
+            Console.WriteLine($"{searchData} does not exist in the Linked List!!!");
+            return false;
+        }
+
         public void Display()
         { 
             Node<T> temp= head;
--- Program.cs
+++ Program.cs
@@ -7,14 +7,20 @@
         {
 
             LinkList<int> l1 = new LinkList<int>();
-            l1.AddFirst(1);
-            l1.AddFirst(2);
-            
-            l1.Display();
 
+            //Adds the element at the very start of list
+            l1.AddFirst(1);
+            l1.AddFirst(9);
+            l1.AddFirst(5);  
+            l1.Display();
+            
+            //Adds the element at last of list
             l1.AddLast(55);
             l1.AddLast(32);
+            l1.Display();
 
+            //Adds the element after a particular element user chooses
+            l1.AddAfter(9,6);
             l1.Display();
 
         }
